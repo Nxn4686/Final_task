85e427aee7aa539ceb1939e1d7d38561bfcf5274
Nikita Lunev <lunevbros@yandex.ru>
2022-10-05 12:36:04 +0300

    Entering strings

--- Programm/Program.cs
+++ Programm/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using static System.Console;
+Clear();
+
+Write("Сколько элементов Вы хотите ввести? ");
+int size = Convert.ToInt32(ReadLine());
+
+string[] arrayStrings = new string[size];
+for (int i = 0; i < size; i++)
+{
+    WriteLine($"Введите {i + 1}-й элемент: ");
+    string element = Convert.ToString(ReadLine());
+    arrayStrings[i] = element;
+}
